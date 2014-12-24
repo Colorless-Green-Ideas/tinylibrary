9015713e04a22a91cfe4b40a24c103901c6f5a8f
jrabbit <jackjrabbit+github@gmail.com>
2014-12-24 17:44:23 -0600

    setup.py first draft license not applicable yet (I haven't decided what to release this as)

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+setup(name="tinylibrary",
+      version="0.1a1",
+      packages=["tinylibrary"],
+      # scripts=[],
+      author="Jack Laxson",
+      author_email="jackjrabbit@gmail.com",
+      description="Simple django app to manage a small library",
+      # license="GPL v2 or later",
+      install_requires=["django", "xmltodict"],
+      url="https://github.com/Colorless-Green-Ideas/tinylibrary",
+      # classifiers=["License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",]
+      )
