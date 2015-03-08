adcb300ab7b34133badee4c1690eeeef5488df32
jrabbit <jackjrabbit+github@gmail.com>
2015-03-07 20:44:37 -0500

    link in title, theme person form, name the inputs

--- tinylibrary/widgets.py
+++ tinylibrary/widgets.py
@@ -7,6 +7,6 @@ class PaperTextInput(TextInput):
     # Unlike inputs using paper-input-decorator directly, 
     # paper-input does not work out of the box with the native form element.
         html = """<paper-input-decorator label='{0}' floatingLabel>
-        <input is="core-input">
+        <input is="core-input" name="{0}">
         </paper-input-decorator>"""
         return format_html(html, name)
