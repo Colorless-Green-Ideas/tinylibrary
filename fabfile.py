ef410f9e2ceb065199e114a7af6211afa8613d1e
Jack Laxson <jackjrabbit@gmail.com>
2018-06-01 05:18:30 -0400

    add flake 8 config, commands to run linters

--- fabfile.py
+++ fabfile.py
@@ -5,6 +5,11 @@ def migrate():
     "run migrations locally"
     local('docker-compose  --log-level ERROR exec web pipenv run python manage.py migrate')
 
+@task
+def lint():
+    with settings(warn_only=True):
+        local("flake8 --count")
+        local("eslint tinylibrary/static/tinylibrary/main.js tinylibrary/static/tinylibrary/tl-scanner.js tinylibrary/static/tinylibrary/quickrete-file.js")
 
 @task
 def mk_migrations():
