048bb2fdf6acf01515dae7a5bf550523d76c5497
Jack Laxson <jackjrabbit@gmail.com>
2018-06-01 05:13:39 -0400

    clean up repo

--- tinylibrary/static/tinylibrary/main.js
+++ tinylibrary/static/tinylibrary/main.js
@@ -3,8 +3,6 @@ import "./main.scss"
 import {Button} from "@material/mwc-button"
 import {Icon} from "@material/mwc-icon"
 
-// import {Card} from "@material/mwc-card"
-
 export default function do_binds() {
     if (document.forms.length > 0) {
     document.querySelector("mwc-button.mwc-submit").addEventListener("click", function(e){
